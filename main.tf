febae31d5539077f9adff91dfdfbdbb657356e55
onyiafranklin <onyiafranklin89@gmail.com>
2022-07-31 23:23:32 +0100

    variable testing

--- main.tf
+++ main.tf
@@ -1,12 +1,37 @@
 provider "aws" {
-  region  = "us-west-2"
+  region  = "us-east-1"
  
 }
 
-resource "aws_instance" "first_server" {
-  ami           = "ami-0d70546e43a941d70"
-  instance_type = "t2.micro"
+variable "my_instance_type" {
+  type        = string
+  description = "This shows the instance type"
+}
+
+resource "aws_vpc" "VPC" {
+  cidr_block = "10.0.0.0/16"
+
+  tags={
+    Name= "production"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.VPC.id
+  cidr_block = "10.0.1.0/24"
+
   tags = {
-    # Name = "Udapeople"
+    Name = "prod-subnet"
+  }
+}
+
+resource "aws_instance" "webserver" {
+  ami = "ami-052efd3df9dad4825"
+  instance_type = var.my_instance_type
+  subnet_id = aws_subnet.subnet.id
+ 
+
+  tags = {
+    Name = "prod-webserver"
   }
 }
